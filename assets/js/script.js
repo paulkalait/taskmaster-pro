5adaafc0473e15626b5371855833b14697c08729
Your Name <kalaitzidispaul@gmail.com>
2022-02-16 20:29:44 -0500

    pushing changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -181,7 +181,54 @@ $(".list-group").on("blur", "textarea", function() {
     .val(text);
   });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+    console.log("activate", this);
+  },
+  over: function(event){
+    console.log("over", event.target);
+  },
+  out: function(event){
+    console.log("out", event.target);
+  },
+  update: function(event){
+    //array to store the task data in
+    var tempArr = [];
 
+    //loop over current set of children in sortable list
+    $(this).children().each(function(){
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      //trim down list's ID to match object property
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      //update array on tasks object and save 
+      tasks[arrName] = tempArr;
+      saveTasks();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim()
+     
+      //add task data to the temp array as an object
+      tempArr.push({
+        text:text,
+        date: date
+      });
+    });
+    console.log(this)
+  }
+})
 
 
 
